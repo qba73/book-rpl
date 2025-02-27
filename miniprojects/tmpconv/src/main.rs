be70e3e5995984a058460b83508ec99fa00b5fa1
Jakub Jarosz <jakub@jarosz.dev>
2025-02-27 19:54:18 +0000

    Temp converter

--- miniprojects/tmpconv/src/main.rs
+++ miniprojects/tmpconv/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Temp COnverter");
+    let celcius:f32 = 28.3;
+    let fahrenheit = c_to_f(celcius);
+    println!("Temp {celcius} in Fahrenheit is: {fahrenheit}");
+
+    let fahreneheit = 70.0;
+    println!("Temp {} in Celcius is: {}", fahreneheit, f_to_c(fahreneheit));
+}
+
+
+fn c_to_f(temp_celcius: f32) -> f32 {
+    temp_celcius * (9.0 / 5.0)
+}
+
+fn f_to_c(temp_fahrenheit: f32) -> f32 {
+    temp_fahrenheit - 32.0 * (5.0/9.0)
 }
