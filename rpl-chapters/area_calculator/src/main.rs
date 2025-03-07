dea7948e9492f5745125dbb6e32bdf81400ff1fa
Jakub Jarosz <jakub@jarosz.dev>
2025-03-07 18:34:09 +0000

    add new methods to impl

--- rpl-chapters/area_calculator/src/main.rs
+++ rpl-chapters/area_calculator/src/main.rs
@@ -7,6 +7,14 @@ fn main() {
 
     let area = area(    &rectangle1);
     println!("the area is {}", area);
+
+    // Print area - call the method on the struct
+    println!("The area is {}", rectangle1.area());
+
+    let rectangle2 = Rectangle{width: 5, height:2};
+
+    let can_hold = rectangle1.can_hold(&rectangle2);
+    println!("Can hold? Answer is: {}.", can_hold);
 }
 
 #[derive(Debug)]
@@ -14,6 +22,19 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+
 
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
